2eae35ea19f1d02371a5c23e794be35d7bb52493
Rorazoro <rorazoro@gmail.com>
2019-11-30 19:02:13 -0500

    Added player with movement scripts

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private CharacterController cc;
+
+    float inputX = Input.GetAxis("Horizontal");
+    float inputY = Input.GetAxis("Vertical");
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            //RaycastHit hit = Physics.Raycast(transform.position, 
+        }
+    }
+}
